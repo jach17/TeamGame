28b35d3f1cae606e247076e00cdfa72dd0fca7e2
Denilson <denicsa211099@gmail.com>
2022-05-28 22:18:17 -0500

    El jugador se mueve y gira.

--- Assets/Deni/Scripts/CameraController.cs
+++ Assets/Deni/Scripts/CameraController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    [SerializeField] float yOffset;
+    private Vector3 vectorOffset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vectorOffset = new Vector3(0, yOffset, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + vectorOffset;
     }
 }
--- Assets/Deni/Scripts/PlayerController.cs
+++ Assets/Deni/Scripts/PlayerController.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] private LayerMask groundMask;
+    private Camera mainCamera;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        this.transform.position += new Vector3(h * speed * Time.deltaTime, 0, v * speed * Time.deltaTime);
+
+        Aim();
+    }
+
+    private void Aim()
+    {
+        var (success, position) = GetMousePosition();
+        if (success)
+        {
+            // Calculate the direction
+            var direction = position - transform.position;
+
+            // You might want to delete this line.
+            // Ignore the height difference.
+            direction.y = 0;
+
+            // Make the transform look in the direction.
+            transform.forward = direction;
+            
+        }
+    }
+
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        {
+            // The Raycast hit something, return with the position.
+            return (success: true, position: hitInfo.point);
+        }
+        else
+        {
+            // The Raycast did not hit anything.
+            return (success: false, position: Vector3.zero);
+        }
     }
 }
